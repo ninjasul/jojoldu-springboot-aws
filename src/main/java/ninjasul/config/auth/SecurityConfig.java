586b6aeb9c6d6e14eeb1c0b7825e324a04a46a93
박동엽 <ninjasul@onestorecorp.com>
2020-08-18 20:13:20 +0900

    SpringSecurity 설정 중

---
+++ src/main/java/ninjasul/config/auth/SecurityConfig.java
@@ -0,0 +1,29 @@
+package ninjasul.config.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.userinfo.CustomUserTypesOAuth2UserService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .headers().frameOptions().disable()
+            .and()
+                .authorizeRequests()
+                .antMatchers(
+        "/",
+                    "/css/**",
+                    "/images/**",
+                    "/js/**",
+                    "/h2-console/**"
+                ).permitAll();
+    }
+}
---
+++ src/main/java/ninjasul/domain/user/Role.java
@@ -0,0 +1,15 @@
+package ninjasul.domain.user;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    // 스프링 시큐리티 권한 코드에는 "ROLE_" 접두어가 있어야 함.
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
+
+    private final String key;
+    private final String title;
+}
