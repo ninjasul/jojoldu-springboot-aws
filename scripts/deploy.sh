693063f459d3462ff2ab7c3574c5c6d46124a045
박동엽 <ninjasul@onestorecorp.com>
2020-08-25 16:12:22 +0900

    CodeDeploy 용 배포 파일 및 Travis CI 설정 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-REPOSITORY=/home/ec2-user/app/step1
+REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=jojoldu-springboot-aws
 
 cd $REPOSITORY/$PROJECT_NAME/
 
-echo "> Git Pull"
-git pull
-
 echo "> 프로젝트 Build 시작"
 ./gradlew build
 
-echo "> stepl 디렉토리로 이동"
-cd $REPOSITORY
-
 echo "> Build 파일 복사"
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 # pgrep 은 process id를 추출하는 명령어, -f 는 full process name 을 가져옴.
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f ${PROJECT_NAME}*.jar)
+CURRENT_PID=$(pgrep -fl jojoldu-springboot-aws.jar | grep jar | awk '{print $1}')
 
 # -z "$CURRENT_PID": 현재 구동 중인 애플리케이션 pid의 길이가 0인 경우. 즉, 존재하지 않는 경우.
 echo "현재 구동 증인 애플리케이션 pid : $CURRENT_PID"
+
 if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동증인 애플리케이션이 없으므로 종료하지 않습니다."
-# kill -9: 프로세스 강제 종료
-# kill -15: 플로세스 정상 종료
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -36,14 +29,18 @@ fi
 # ls -tr: 수정시간 역순으로 파일과 디렉토리 목록 조회
 # grep *.jar | tail -n 1: jar 파일 중 가장 마지막에 있는 항목 조회
 echo "> 새 애플리케이션 배포"
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep *.jar | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 # java -jar jar파일: 일반적인 자바프로그램 실행, 사용자가 터미널 접속을 끊을 때 애플리케이션도 같이 종료됨.
 # nohup java -jar: 애플리케이션 실행자가 터미널을 종료해도 애플리케이션은 계속 구동.
 # 2>&1: 표준 오류가 표준 output stream 을 통해 출력됨.
 echo "> JAR Name: $JAR_NAME"
 
+chmod +x $JAR_NAME
+
+ccho "> $JAR_NAME 실행"
+
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $REPOSITORY/$JAR_NAME 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
