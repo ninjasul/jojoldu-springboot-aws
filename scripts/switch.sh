94c78ae64da7cdea1e2188d2ff0220393d6f2d23
1000141 <ninjasul@onestorecorp.com>
2020-09-02 19:38:53 +0900

    switch.sh 오류 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -12,8 +12,7 @@ function switch_proxy() {
 
   # echo 'set \$service_url http://127.0.0.1:${IDLE_PORT};': 하나의 문장을 만들어 파이프라인(|)으로 넘겨줌.
   # sudo tee /etc/nginx/conf.d/service-url.inc: 앞에서 넘겨준 문자열을 service-url.inc 에 덮어씀.
-  echo 'set \$service_url http://127.0.0.1:${IDLE_PORT};' | sudo tee /etc/nginx/conf.d/service-url.inc
-
+  echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   # restart는 서비스가 잠시 중단됨. reload는 중단 없이 재시작.
   # 중요한 설정들은 반영되지 않으므로 restart를 사용해야 함.
   # 이 예제에서는 service-url 설정 값을 외부 설정 파일로 다시 불러오는 것이므로 reload 로 가능.
